2bfe8a022318d1d44bc066649a5dc94c974f2abc
eliot guihal <eliot.guihal@ynov.com>
2023-11-13 15:53:45 +0000

    2.0

--- src/users/auth/auth.service.ts
+++ src/users/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, UnauthorizedException, Inject, forwardRef } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  Inject,
+  forwardRef,
+} from '@nestjs/common';
 import { UsersService } from '../users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as Argon2 from 'argon2';
@@ -7,12 +12,12 @@ import * as Argon2 from 'argon2';
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
   ) {}
 
   async signIn(username, pass) {
     const user = await this.usersService.findByUsername(username);
-    if (!user || await Argon2.verify(user.password, pass)) {
+    if (!user || (await Argon2.verify(user.password, pass))) {
       throw new UnauthorizedException();
     }
     const payload = { sub: user.id, username: user.username };
